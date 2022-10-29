c1ff337d1cd8dcfde9dbb751c28c1232469a7a12
Vasko Nikolov <hugovasko1@gmail.com>
2022-10-29 15:10:55 +0300

    Main - Project init

---
+++ PuppyCare/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var remainingFood = int.Parse(Console.ReadLine()) * 1000;
+
+var command = Console.ReadLine();
+
+while (command != "Adopted")
+{
+    remainingFood -= int.Parse(command);
+    command = Console.ReadLine();
+}
+
+if (remainingFood >= 0)
+    Console.WriteLine($"Food is enough! Leftovers: {remainingFood} grams.");
+else
+    Console.WriteLine($"Food is not enough. You need {Math.Abs(remainingFood)} grams more.");
